91fff383c509825b920eab56ae5735afec5ddee9
rafael nascimento <rafanascimento.au@gmail.com>
2020-08-15 03:41:17 +1000

    import and style social media icons

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import FooterNav from '../Footer/FooterNav/FooterNav';
 import Logo from '../Logo/Logo';
+import Face from '../../assets/images/socialMedia/face.svg';
+import Insta from '../../assets/images/socialMedia/insta.svg';
+import Pint from '../../assets/images/socialMedia/pint.svg';
+import Twitter from '../../assets/images/socialMedia/twitter.svg';
+import Youtube from '../../assets/images/socialMedia/youtube.svg';
+// import FontAwesome from  '@fortawesome/fontawesome-svg-core';
+// import { faCode, faHighlighter } from '@fortawesome/free-solid-svg-icons';
 
 import classes from './Footer.module.css';
+
+const Icons = [Face, Insta, Pint, Twitter, Youtube]
 
 const Footer = () => (
     <>
@@ -14,13 +23,14 @@ const Footer = () => (
         </p>
         <FooterNav></FooterNav>
     </div>
-    <div>
-    <ul>
-                    <li>face</li>
-                    <li>face</li>
-                    <li>face</li>
-                    <li>face</li>
-                </ul>
+    <div className={classes.socialIcon}>
+    
+                    <div className={classes.socialBoxText}><p>share. connect. follow</p></div>
+                    {Icons.map(src => (
+            <img alt="" key={src} src={src} />
+          ))}
+                    
+               
     </div>
     <div>
        
