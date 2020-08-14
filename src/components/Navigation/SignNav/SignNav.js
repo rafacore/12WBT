4878dc5e47c2d901646f8027a9316f1ea2d7b4b9
rafael nascimento <rafanascimento.au@gmail.com>
2020-08-14 18:03:07 +1000

    creating sign  component

---
+++ src/components/Navigation/SignNav/SignNav.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import classes from './SignNav.module.css';
+
+const SignNav = () =>(
+ <div className={classes.SignNav}>
+          <a href=" " >Login</a>
+            <a href=" " className={classes.JoinNowBtn}>Join Now</a>
+    </div>
+)
+
+export default SignNav;
