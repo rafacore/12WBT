455d9379e31b5d49432f72fb18cbeff993bb4f46
rafael nascimento <rafanascimento.au@gmail.com>
2020-08-14 21:47:57 +1000

    creating slides  component

---
+++ src/components/UI/Slides/Slides.js
@@ -0,0 +1,47 @@
+import React, {useState} from 'react';
+import ReactSlidy from 'react-slidy';
+import 'react-slidy/lib/styles.css';
+import img from '../../../assets/images/home/carousel/BA-Sue-1.jpeg';
+
+
+
+const SLIDES = [img, img, img,img, img, img]
+const createStyles = isActive => ({
+  background: 'transparent',
+  border: 0,
+  color: isActive ? '#333' : '#ccc',
+  cursor: 'pointer',
+  fontSize: '32px'
+})
+
+const Slides =() =>{
+    const [actualSlide, setActualSlide] = useState(0)
+    const updateSlide = ({currentSlide}) => {
+      setActualSlide(currentSlide)
+    }
+    return (
+      <>
+        <ReactSlidy numOfSlides={3} doAfterSlide={updateSlide} slide={actualSlide}>
+          {SLIDES.map(src => (
+            <img alt="" key={src} src={src} />
+          ))}
+        </ReactSlidy>
+        <div className="Dots">
+          {SLIDES.map((_, index) => {
+            return (
+              <button
+                key={index}
+                style={createStyles(index === actualSlide)}
+                onClick={() => updateSlide({ currentSlide: index })}>
+                &bull;
+              </button>
+            )
+          })}
+        </div>
+      </>
+    )
+  }
+
+
+
+export default Slides;
