33d0186e7a7bc185195ae22f94e1a7ad77e4fe5e
rafael nascimento <rafanascimento.au@gmail.com>
2020-08-15 04:54:21 +1000

    social media   improvement

--- src/components/Home/SocialMedia/SocialMedia.js
+++ src/components/Home/SocialMedia/SocialMedia.js
@@ -1,23 +1,30 @@
 import React from 'react';
 import classes from './SocialMedia.module.css';
+import Face from '../../../assets/images/socialMedia/face.svg';
+import Insta from '../../../assets/images/socialMedia/insta.svg';
+import Pint from '../../../assets/images/socialMedia/pint.svg';
+import Twitter from '../../../assets/images/socialMedia/twitter.svg';
+import Youtube from '../../../assets/images/socialMedia/youtube.svg';
 
+const Icons = [Face, Insta, Twitter, Youtube]
 
 const SocialMedia = ( props ) =>(
-    <>
+    <div className={classes.socialContent}>
     <div>
-    <p>{props.title}</p>
+    <h2 className={classes.title}>{props.title}</h2>
 </div>
 <div className={classes.socialIconBox}>
 
-    <div>face</div>
-    <div>face</div>
-    <div>face</div>
-    <div>face</div>
+    {Icons.map(src => (
+<img alt="" key={src} src={src} />
+))}
+    
+
     
    
 
 </div>
-</>
+</div>
 );
 
 export default SocialMedia;
