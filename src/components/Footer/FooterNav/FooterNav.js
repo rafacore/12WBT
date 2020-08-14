c585ca1619baaf83fa3e5b4113fc454e792d0f3d
rafael nascimento <rafanascimento.au@gmail.com>
2020-08-14 17:57:21 +1000

    creating footer navigation  component

---
+++ src/components/Footer/FooterNav/FooterNav.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+
+const FooterNav = () => (
+    <div>
+    <div>
+        <p>Consult your healthcare professional before beginning any diet or fitness regime.
+            Copyright © 2020 12WBT Trading Pty Ltd.
+        </p>
+    </div>
+    <div>
+        <ul>
+            <li>12WBT Blog|</li>
+            <li>Support Crew|</li>
+            <li>Careers|</li>
+            <li>Contact Us|</li>
+            <li>Terms|Privacy</li>
+        </ul>
+        <p>* Results may vary. Exercise and proper diet are necessary to achieve and maintain weight loss.
+
+        <a href="">** View full competition terms and conditions here.</a></p>
+    </div>
+
+</div>
+
+
+);
+
+export default FooterNav;
