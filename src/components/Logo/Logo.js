eb95c5e8bf68029a799cbcb571163ed1d33d4b58
rafael nascimento <rafanascimento.au@gmail.com>
2020-08-15 01:17:45 +1000

    adapting logo to footer

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -2,9 +2,11 @@ import React from 'react';
 import classes from './Logo.module.css';
 import LogoImg from '../../assets/images/Logo.svg';
 
+
+
 const Logo = ( props ) => (
 
-    <div className={classes.logo}><img src={LogoImg} /></div>
+    <div className={!props.isFooter ? classes.logo : classes.logoF}><img src={LogoImg} /></div>
 );
 
 export default Logo;
