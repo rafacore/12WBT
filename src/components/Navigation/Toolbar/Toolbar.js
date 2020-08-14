1e6c37f02a836a50e3134c3a8bc91fc339397117
rafael nascimento <rafanascimento.au@gmail.com>
2020-08-14 17:45:55 +1000

    creating toolbar  component

---
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import Logo from '../../Logo/Logo';
+import NavigationItems from '../NavigationItems/NavigationItems';
+import SignNav from '../SignNav/SignNav';
+import classes from './Toolbar.module.css';
+
+
+const Toolbar = ( props ) =>(
+   
+    <div className={classes.Toolbar}>
+       
+        <Logo />
+       
+        <NavigationItems></NavigationItems>
+        
+        
+        <SignNav />
+          
+    </div>
+  
+);
+
+
+export default Toolbar;
+    
